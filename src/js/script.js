3b10e58557c5165361589105942579c127b1e844
Andrew <Papish_krolser@mail.ru>
2023-04-25 11:36:48 +0300

    finish adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -5,12 +5,30 @@ $('a[href^="#"]').click(function(){
   $('html, body').animate({scrollTop: $(target).offset().top}, 800);
   return false;
   });
-//Гамбургер и адаптивное меню
+
 
 window.addEventListener('DOMContentLoaded', () => {
   const menu = document.querySelector('.menu__list'),
   menuItem = document.querySelectorAll('.menu__item'),
   hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu__list_active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('menu__list_active');
+      })
+  })
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.menu__list_footer'),
+  menuItem = document.querySelectorAll('.menu__item_footer'),
+  hamburger = document.querySelector('.hamburger_footer');
 
   hamburger.addEventListener('click', () => {
       hamburger.classList.toggle('hamburger_active');
